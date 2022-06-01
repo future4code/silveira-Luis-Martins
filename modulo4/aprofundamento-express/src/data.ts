91b3dd8528ffe9712ffda4ad807a03d90b9df8f6
Luis Gustavo Martins <lisgustavo@gmail.com>
2022-06-01 13:18:25 -0300

    Aprofundamento-Express

---
+++ modulo4/aprofundamento-express/src/data.ts
@@ -0,0 +1,27 @@
+type afazeres = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
+
+export const afazeres: afazeres[] = [
+  {
+      "userId": 1,
+      "id": 1,
+      "title": 'Acordar',
+      "completed": true
+  },
+  {
+      "userId": 2,
+      "id": 2,
+      "title": 'Tomar café da manhã',
+      "completed": true
+  },
+  {
+      "userId": 3,
+      "id": 3,
+      "title": 'Fazer atividade física',
+      "completed": false
+  }
+];
