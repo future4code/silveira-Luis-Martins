73a3b742cf82823454a07b74796b68aa733742f8
Luis Gustavo Martins <lisgustavo@gmail.com>
2022-06-01 13:43:51 -0300

    fluxo de dados no back

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,29 @@
+type Product = {
+  id: string
+  name: string
+  price: number
+}[]
+
+export const product: Product =
+  [
+      {
+          "id": "721361723",
+          "name": "Camiseta CK",
+          "price": 120.90
+      },
+      {
+          "id": "3216532167",
+          "name": "Perfume Boticário",
+          "price": 180.90
+      },
+      {
+          "id": "1263572123",
+          "name": "Relógio Invicta",
+          "price": 320.90
+      },
+      {
+          "id": "2138712389",
+          "name": "Calça CK",
+          "price": 230.90
+      }
+  ] 
