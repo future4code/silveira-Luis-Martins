066cf85bf84946b780edbf254bcc4d5052cd42a1
Luis Gustavo Martins <lisgustavo@gmail.com>
2022-07-04 00:33:47 -0300

    Projeto Cookenu

---
+++ modulo5/Projeto-Cookenu/src/index.ts
@@ -0,0 +1,11 @@
+import {app} from "./app"
+import signup from "./endpoints/users/signup"
+
+
+app.post("/users/signup", signup)
+app.post("/users/login")
+app.get("/users/profile")
+app.get("/users/:id/profile")
+
+app.post("/recipe")
+app.get("/recipe/:id")
