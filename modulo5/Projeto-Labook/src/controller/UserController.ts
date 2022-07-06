637096a1397e93dfbab26653f033a44b3bca42d9
Luis Gustavo Martins <lisgustavo@gmail.com>
2022-07-05 23:55:48 -0300

    inicio do projeto

---
+++ modulo5/Projeto-Labook/src/controller/UserController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import UserBusiness from "../business/UserBusiness";
+import { SignupInputDTO } from "../types/signupInputDTO";
+
+export default class UserController{
+    constructor(
+        private userBusiness: UserBusiness
+    ){}
+
+    signup = async(req: Request, res: Response) =>{
+        const {name, email, password} = req.body;
+
+        const input: SignupInputDTO ={
+            name,
+            email,
+            password
+        }
+        try {
+            const token = await this.userBusiness.signup(input)
+            res.status(201).send({message: "Usuário cadastrado com sucesso", token})
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).send(error.message)
+            }
+            res.status(500).send("Erro no signup")
+        }
+    }
+}
